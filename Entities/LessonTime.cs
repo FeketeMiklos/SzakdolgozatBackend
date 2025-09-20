6e4869e4741800ae39a298c0f750509c03fe44f1
Fekete Miklós <113507853+FeketeMiklos@users.noreply.github.com>
2025-09-20 15:49:39 +0200

    added database entities + DBcontext

---
+++ Entities/LessonTime.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SzakdolgozatBackend.Entities
+{
+    public class LessonTime
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public DateOnly Date { get; set; }
+        public TimeOnly StartTime { get; set; }
+        public TimeOnly EndTime { get; set; }
+        public int LessonId { get; set; }
+        public Lesson Lesson { get; set; }
+    }
+}
---
+++ Entities/User.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SzakdolgozatBackend.Entities
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        public string PasswordHash { get; set; }
+
+        public UserTypeEnum Type { get; set; }
+
+        public List<Lesson> Lessons { get; set; }
+    }
+}
